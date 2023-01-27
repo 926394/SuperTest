6837104e51be2fe47a323533e9f0e6ac87a624b4
Nikolay <meta1mang@mail.ru>
2023-01-27 16:01:57 +0300

    создано 2 метода для приема и вывода значений в массив с типом string

---
+++ Test/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+string[] GetMassivNum(string[] coll)
+{
+    for(int index = 0; index < coll.Length; index ++)
+    {
+        Console.Write($"Введите {index+1} значеие из нескольких символов: ");
+        coll[index] = Console.ReadLine()!;
+    }
+    return coll;
+}
+
+void TheFinishNum(string[] collection)
+{
+    for(int index = 0; index < collection.Length; index ++)
+    {
+        Console.Write($"{collection[index]} ");  
+    }
+    Console.WriteLine();
+}
+
+string[] array = new string[2];
+
+GetMassivNum(array);
+Console.WriteLine();
+TheFinishNum(array);
+Console.WriteLine();
